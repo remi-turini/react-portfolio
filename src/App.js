86e1884508f246b24e558babae22eafc2dd946e2
Remi <remi.turini@epitech.eu>
2021-05-06 01:57:21 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div class="container">
-      
-    </div>
+    <Navbar></Navbar>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (      
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Rémi TURINI</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Mes projets <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">A propos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+    )
+}
+
+export default Navbar
